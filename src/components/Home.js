0318383dec8a79f56223ddcc8a04a1eb74776e62
swynnejr <saucersam@gmail.com>
2021-09-01 18:30:34 -0500

    Established Home Div

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+// Components
+
+// Hook
+
+// Image
+import noImage from '../images/no_image.jpg';
+
+const Home = () => {
+    const [state, setState] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    return <div>Home Page</div>
+}
+
+export default Home;
