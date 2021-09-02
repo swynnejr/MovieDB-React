f52995e88cf7eed73ee0ddafefe1f207150e3685
swynnejr <saucersam@gmail.com>
2021-09-01 21:23:11 -0500

    API pulls successfully

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// API
+import API from '../API';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
@@ -13,8 +15,34 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            // console.log(movies);
+
+
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }))
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    // Initial Render
+    useEffect(() => {
+        fetchMovies(1)
+    }, [])
+
+    console.log(state);
 
     return <div>Home Page</div>
-}
+};
 
 export default Home;
