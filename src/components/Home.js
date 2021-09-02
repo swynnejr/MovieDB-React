0586e864098a0da91c0b8bc1fe7457770229879c
swynnejr <saucersam@gmail.com>
2021-09-01 21:47:07 -0500

    4 api calls instead of 8 (initialState?)

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,40 +6,12 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 
 // Hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
-import noImage from '../images/no_image.jpg';
+import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-            // console.log(movies);
-
-
-            setState(prev => ({
-                ...movies,
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    };
-
-    // Initial Render
-    useEffect(() => {
-        fetchMovies(1)
-    }, [])
-
+    const { state, loading, error } = useHomeFetch();
     console.log(state);
 
     return <div>Home Page</div>
