c78c88048ce6b39ce3cc8c00afb0735fe6b83ab7
swynnejr <saucersam@gmail.com>
2021-09-02 23:06:07 -0500

    Movie info page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,8 +3,10 @@ import { useParams } from 'react-router-dom';
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
+import MovieInfo from './MovieInfo'
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image
@@ -15,12 +17,16 @@ const Movie = () => {
 
 
     const { state: movie, loading, error } = useMovieFetch(movieId)
-    console.log(movie);
+    
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong...</div>;
+
     return (
         <>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title} />
+        <MovieInfo movie={movie} />
         </>
-    )
+    );
 
 };
 
